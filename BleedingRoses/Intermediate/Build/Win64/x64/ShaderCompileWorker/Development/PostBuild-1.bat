f0c1dcc73a7b688ab5d7041522da62aa6588821c
Preston Peek <36671440+dprestonpeek@users.noreply.github.com>
2025-06-27 13:02:49 -0400

    Fix gitignore

--- BleedingRoses/Intermediate/Build/Win64/x64/ShaderCompileWorker/Development/PostBuild-1.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-echo Copying C:\Users\dpres\Documents\Projects\UnrealAgain\Engine\Binaries\Win64\ShaderCompileWorker.exe to C:\Users\dpres\Documents\Projects\UnrealAgain\Engine\Binaries\Win64\XGEControlWorker.exe
-copy /Y /B "C:\Users\dpres\Documents\Projects\UnrealAgain\Engine\Binaries\Win64\ShaderCompileWorker.exe" /B "C:\Users\dpres\Documents\Projects\UnrealAgain\Engine\Binaries\Win64\XGEControlWorker.exe" >nul:
